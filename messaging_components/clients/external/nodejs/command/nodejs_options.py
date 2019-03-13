fc88d1fa6a581ffe133249f275f7c3e9c4dc4717
Fernando Giorgetti <fgiorget@redhat.com>
2019-03-13 14:43:56 -0300

    UPD(issue #18) - Added initial support for --on-release on cli-rhea-sender

--- messaging_components/clients/external/nodejs/command/nodejs_options.py
+++ messaging_components/clients/external/nodejs/command/nodejs_options.py
@@ -33,12 +33,31 @@ class NodeJSControlOptionsSenderReceiver(ControlOptionsSenderReceiver, NodeJSCon
         ControlOptionsSenderReceiver.__init__(self, duration=duration, duration_mode=duration_mode,
                                               capacity=capacity)
         NodeJSControlOptionsCommon.__init__(self, broker=broker, count=count, timeout=timeout,
-                                          sync_mode=sync_mode, close_sleep=close_sleep)
+                                            sync_mode=sync_mode, close_sleep=close_sleep)
         self.address = address  # type: str
 
     def valid_options(self) -> list:
         return NodeJSControlOptionsCommon.valid_options(self) + [
             Prefixed('address', '--address')
+        ]
+
+
+class NodeJSControlOptionsSender(NodeJSControlOptionsSenderReceiver):
+    """
+    Specialized implementation of control options for cli-rhea Sender and Receiver client commands.
+    """
+    def __init__(self, broker: str='localhost:5672', address: str='examples', count: int=1,
+                 timeout: int=None, sync_mode: str=None, close_sleep: int=None,
+                 duration: int=None, duration_mode: str=None, capacity: int=None,
+                 on_release: str='retry'):
+        NodeJSControlOptionsSenderReceiver.__init__(self, broker=broker, count=count, timeout=timeout,
+                                                    sync_mode=sync_mode, duration=duration,
+                                                    duration_mode=duration_mode, capacity=capacity)
+        self.on_release = on_release  # type: str
+
+    def valid_options(self) -> list:
+        return NodeJSControlOptionsSenderReceiver.valid_options(self) + [
+            Prefixed('on-release', '--on-release')
         ]
 
 
@@ -52,8 +71,8 @@ class NodeJSControlOptionsReceiver(ControlOptionsReceiver, NodeJSControlOptionsS
                  dynamic: bool=False):
         ControlOptionsReceiver.__init__(self, dynamic=dynamic)
         NodeJSControlOptionsSenderReceiver.__init__(self, broker=broker, count=count, timeout=timeout,
-                                                  sync_mode=sync_mode, duration=duration,
-                                                  duration_mode=duration_mode, capacity=capacity)
+                                                    sync_mode=sync_mode, duration=duration,
+                                                    duration_mode=duration_mode, capacity=capacity)
 
 
 class NodeJSConnectionOptionsCommon(ConnectionOptionsCommon):
